5fea10772ab0e7ebd65ab192069f0cc6deda152a
Noah Too <krivahtoo@gmail.com>
2021-11-28 03:08:08 +0300

    🎆 Finally tiles render as expected

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,8 @@
 use std::fs::File;
-use std::io::{Result, Write, BufWriter};
+use std::io::{Result, Write};
 use rand::seq::SliceRandom;
 
-#[derive(Clone, Copy)]
+#[derive(Clone, Copy, Debug)]
 enum TileType {
   North = 1,
   East = 2,
@@ -10,6 +10,7 @@ enum TileType {
   West = 8
 }
 
+#[derive(Debug)]
 struct Tile(Option<TileType>,Option<TileType>,Option<TileType>,Option<TileType>);
 
 use TileType::*;
@@ -34,6 +35,7 @@ impl Tile {
     }
     Self(n,e,s,w)
   }
+  /// Get possible tiles on edge for the current tile
   fn get_possible(&self,edge: u8) -> Vec<u8> {
     let mut res: Vec<u8> = Vec::new();
     if edge >= 4 { panic!("Edge out of range") }
@@ -41,22 +43,21 @@ impl Tile {
     for x in 0..16 {
       let tile = Self::new(x);
       match n {
-        0 if type_eq(&self.0, &tile.0) => {
+        0 if type_eq(&self.2, &tile.0) => {
             res.push(x as u8);
         },
-        1 if type_eq(&self.1, &tile.1) => {
+        1 if type_eq(&self.3, &tile.1) => {
             res.push(x as u8);
         },
-        2 if type_eq(&self.2, &tile.2) => {
+        2 if type_eq(&self.0, &tile.2) => {
             res.push(x as u8);
         },
-        3 if type_eq(&self.3, &tile.3) => {
+        3 if type_eq(&self.1, &tile.3) => {
             res.push(x as u8);
         },
         _ => (),
       }
     }
-    println!("{} -> {} => {:?}", &edge, &n, &res);
     res
   }
   fn to_u8(&self) -> u8 {
@@ -85,7 +86,6 @@ fn type_eq(a: &Option<TileType>, b: &Option<TileType>) -> bool {
   }
 }
 
-
 struct RGB(u8,u8,u8);
 
 impl RGB {
@@ -94,11 +94,12 @@ impl RGB {
   }
 }
 
+/// Return a randon value in vs
 fn get_rand(vs: &[u8]) -> u8 {
   *(vs.choose(&mut rand::thread_rng()).unwrap())
 }
 
-// Returns true if the i'th bit of x is set to 1.
+/// Returns true if the i'th bit of x is set to 1.
 fn get_bit(x: u32, i: i32) -> bool {
   (x >> i) & 1 != 0
 }
@@ -121,19 +122,20 @@ fn save_as_ppm(file_path: &str, pixels: &[u32], width: usize, height: usize) -> 
   Ok(())
 }
 
-fn draw_circle(u: f32, v: f32) -> RGB {
-  let c = (0.5, 0.5);
-  let r = 0.25;
-  let dx = c.0 - u;
-  let dy = c.1 - v;
-  if dx*dx + dy*dy <= r*r {
-    RGB(0x00,0x0a,0x00)
-  } else {
-    RGB(0xff, 0xff, 0xff)
-  }
-}
+// fn draw_circle(u: f32, v: f32) -> RGB {
+//   let c = (0.5, 0.5);
+//   let r = 0.25;
+//   let dx = c.0 - u;
+//   let dy = c.1 - v;
+//   if dx*dx + dy*dy <= r*r {
+//     RGB(0x00,0x0a,0x00)
+//   } else {
+//     RGB(0xff, 0xff, 0xff)
+//   }
+// }
 
-// u for x axis, v for y axis
+/// Draw tiles
+/// u for x axis, v for y axis, n for tile number 0-15
 fn draw(u: f32,v: f32,n: u8) -> RGB {
   let c = 0.5;
   let res: bool = match n {
@@ -153,7 +155,7 @@ fn draw(u: f32,v: f32,n: u8) -> RGB {
     13 => (v >= u) || (u >= v && v <= c && u <= 1.0 - v),
     14 => (v >= u) || (v <= u && u >= 1.0 - v),
     15 => true,
-    _ => panic!("Value out of range"),
+    _ => panic!("Tile number out of range"),
   };
   if res {
     RGB(0x00,0x0a,0x00)
@@ -169,7 +171,6 @@ fn main() {
   const TILES_W: usize = 16;
   const OUTPUT_PATH: &str = "output.ppm";
   let mut pixels = [0u32; WIDTH*HEIGHT];
-  let tiles: usize = TILES_H*TILES_W;
   let tileset: [u8; 16] = [
     0b0000, // 0
     0b0001, // 1
@@ -189,43 +190,56 @@ fn main() {
     0b1111  // 15
   ];
   let mut next_tile: usize = get_rand(&tileset) as usize;
-  let mut last_x_start_tile = Tile::new(next_tile);
   let mut last_x_tiles: Vec<Tile> = Vec::new();
   for y in 0..TILES_H {
     let mut x_buffer: Vec<Tile> = Vec::new();
     for x in 0..TILES_W {
-      let mut num: u8 = 0;
-      let mut vs: Vec<u8> = Vec::new();
-      let mut tile: Tile = Tile::new(next_tile);
-      if x == 0 && y > 0 {
-        vs = last_x_start_tile.get_possible(2);
-        tile = Tile::new(get_rand(&vs) as usize);
-      } else if y > 0 {
-      } else {
-        vs = tile.get_possible(1);
-      }
-      num = tile.to_u8();
+      let vs: Vec<u8>;
+      let tile: Tile = Tile::new(next_tile);
       for y1 in 0..(HEIGHT/TILES_H) {
         for x1 in 0..(WIDTH/TILES_W) {
           let u = (x1 as f32)/((WIDTH/TILES_W) as f32);
           let v = (y1 as f32)/((HEIGHT/TILES_H) as f32);
           pixels[
             ((y*HEIGHT/TILES_H)+y1)*WIDTH+((x*WIDTH/TILES_W)+x1)
-          // ] = draw_circle(u, v);
-          ] = draw(u, v, num).to_u32();
+          ] = draw(u, v, tile.to_u8()).to_u32();
         }
       }
-      if x == 0 {
-        last_x_start_tile = Tile::new(num as usize);
+      // TODO: Try create an endless tilesets.
+      // get the next tile
+      if y > 0 && x != (TILES_W-1) {
+        let vs1: Vec<u8> = last_x_tiles[x+1].get_possible(2);
+        let vs2: Vec<u8> = tile.get_possible(1);
+        vs = vs2
+          .iter()
+          .filter(|v| vs1.contains(*v))
+          .map(|v| *v)
+          .collect::<Vec<u8>>();
+      } else if x == (TILES_W-1) {
+        vs = x_buffer[0].get_possible(2);
+      } else {
+        vs = tile.get_possible(1);
       }
-      vs = tile.get_possible(1);
-      x_buffer.push(Tile::new(num as usize));
-      println!("{:?} -> {}", &vs, &next_tile);
+      // cache current row value
+      x_buffer.push(Tile::new(tile.to_u8() as usize));
       next_tile = get_rand(&vs) as usize;
     }
     last_x_tiles = x_buffer;
   }
-  println!("{}", WIDTH / TILES_W);
-  println!("{}", West as u8);
   save_as_ppm(OUTPUT_PATH, &pixels, WIDTH, HEIGHT).unwrap();
 }
+
+#[test]
+fn test_tile_possible_values_bottom() {
+  let tile = Tile::new(4);
+  let vs = tile.get_possible(2);
+  assert!(&vs == &[1, 3, 5, 7, 9, 11, 13, 15]);
+}
+
+#[test]
+fn test_tile_possible_values_left() {
+  let tile = Tile::new(4);
+  let vs = tile.get_possible(1);
+  assert!(&vs == &[0, 1, 2, 3, 4, 5, 6, 7]);
+}
+
